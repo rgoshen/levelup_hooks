99a51af53ee45c44119b63808ed060a5042015c3
Richard Goshen <rick.goshen@gmail.com>
2022-07-07 17:15:24 -0700

    copy app.js and reset

--- src/App copy 3.js
+++ src/App copy 3.js
@@ -1,16 +1,30 @@
-import React, { useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Toggle from './Toggle';
 import useTitleInput from './hooks/useTitleInput';
 import useAbortableFetch from 'use-abortable-fetch';
 
 const App = () => {
   const [name, setName] = useTitleInput('');
+  // const [dishes, setDishes] = useState([]);
   const ref = useRef();
-  const { data } = useAbortableFetch(
+  const { data, loading } = useAbortableFetch(
     'https://my-json-server.typicode.com/leveluptuts/fakeapi/dishes'
   );
 
   if (!data) return null;
+
+  // const fetchDishes = async () => {
+  //   console.log('fetching dishes...');
+  //   const res = await fetch(
+  //     'https://my-json-server.typicode.com/leveluptuts/fakeapi/dishes'
+  //   );
+  //   const data = await res.json();
+  //   setDishes(data);
+  // };
+
+  // useEffect(() => {
+  //   fetchDishes();
+  // }, []);
 
   return (
     <div className='main-wrapper' ref={ref}>
@@ -30,6 +44,7 @@ const App = () => {
         />
         <button>Submit</button>
       </form>
+      {/* {dishes.map((dish) => ( */}
       {data.map((dish) => (
         <article className='dish-card dish-card--withImage' key={dish.name}>
           <h3>{dish.name}</h3>
